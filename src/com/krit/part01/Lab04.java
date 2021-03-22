a343a16e71103faef9b88bb2c5ac0b36e7391b33
Krit <kritdevelop@gmail.com>
2021-03-22 16:09:41 +0700

    Part01 - Lab04

---
+++ src/com/krit/part01/Lab04.java
@@ -0,0 +1,19 @@
+package com.krit.part01;
+
+public class Lab04 {
+
+	public static void main(String[] args) {
+		int score = 30;
+		boolean isHandSome = false;
+		
+		if (score >= 80 || isHandSome) {
+			System.out.println("Good");
+		} else if (score >= 50) {
+			System.out.println("Normal");
+		} else {
+			System.out.println("Fail");
+		}
+
+	}
+
+}
