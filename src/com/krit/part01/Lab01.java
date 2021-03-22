cbd63d16407591cc44e5460973c1bfc168f212fb
Krit <kritdevelop@gmail.com>
2021-03-22 15:54:23 +0700

    Part01 - Lab01

---
+++ src/com/krit/part01/Lab01.java
@@ -0,0 +1,25 @@
+package com.krit.part01;
+
+public class Lab01 {
+
+	public static void main(String[] args) {
+		// Single Line Comment
+		/*
+		   Multiple Line Comment
+		   Multiple Line Comment
+		   Multiple Line Comment
+		 */
+		int i = 10;
+		boolean b = true;
+		String s = "Hello Java";
+		float f = 5.99f;
+		char c = 'C';
+		
+		System.out.println("This is a int " + i);
+		System.out.println("This is a boolean " + b);
+		System.out.println("This is a String " + s);
+		System.out.println("This is a float " + f);
+		System.out.println("This is a char " + c);
+	}
+
+}
