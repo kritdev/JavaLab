2fcfbca90c5bb01d433f406b180e3ff06fc03ba3
Krit <kritdevelop@gmail.com>
2021-03-23 16:10:12 +0700

    Part03 - Lab interface

--- src/com/krit/part03/Employee.java
+++ src/com/krit/part03/Employee.java
@@ -5,10 +5,23 @@ public class Employee {
   public String lastname;
   private int salary;
 
+  public String position;
+
   public Employee(String firstnameInput, String lastnameInput, int salaryInput) {
     this.firstname = firstnameInput;
     this.lastname = lastnameInput;
     this.salary = salaryInput;
+    this.position = "Stuff";
+  }
+
+  public Employee(String firstname, String lastname, int salary, String position) {
+    this(firstname, lastname, salary);
+    this.position = position;
+  }
+
+  public static void main(String[] args) {
+    Employee employee = new Employee("Mike", "Yello", 20000, "Messenger");
+    employee.checkPosition();
   }
 
   public void hello() {
@@ -17,5 +30,9 @@ public class Employee {
 
   public int getSalary() {
     return salary;
+  }
+
+  public void checkPosition() {
+    System.out.println("My Position is " + position);
   }
 }
